4d099acabf67bf663dd44fb0f7d958954ddcb966
Vinesh Kamble <126277211+vinesh997@users.noreply.github.com>
2025-05-11 20:51:59 +0530

    Add files via upload

---
+++ cutadapt2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+folder="Directory of the raw files"
+
+mkdir "${folder}/cutadapt2"
+
+for i in $(ls ${folder}/fastq_files2/*.gz | xargs -n 1 basename | sed 's/\(.*\)_.*/\1/' | sed 's/\(.*\)_.*/\1/' | sort -u)
+
+do
+echo ${i}
+
+# Cutadapt
+
+(cutadapt -j 30 -m 151 ${folder}/fastq_files2/${i}_R1.fastq.gz ${folder}/fastq_files2/${i}_R2.fastq.gz -o ${folder}/cutadapt2/${i}_R1.fastq.gz -p ${folder}/cutadapt2/${i}_R2.fastq.gz)2>> ${folder}/cutadapt2/cutadapt.log
+
+done
---
+++ hisat2_2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+folder="Directory of the raw files"
+hisat2="Directory for hisat2-2.1.0"
+
+mkdir "${folder}/hisat2"
+
+# Human genome index
+genome="Directory of human genome "
+
+for i in $(ls ${folder}/cutadapt2/*.gz | xargs -n 1 basename | sed 's/\(.*\)_.*/\1/' | sed 's/\(.*\)_.*/\1/' | sort -u)
+
+do
+echo ${i}
+
+echo -e "\n""Sample ${i} alignment">>${folder}/hisat2/hisat2.log
+
+# for human
+(${hisat2}/hisat2 -p 20 -5 15 -3 5 --dta --known-splicesite-infile ${genome}/gencode.v31.annotation_splicesites.txt -x ${genome}/GRCh38.p12.genome -1 ${folder}/cutadapt/${i}_R1.fastq.gz,${folder}/cutadapt2/${i}_R1.fastq.gz -2 ${folder}/cutadapt/${i}_R2.fastq.gz,${folder}/cutadapt2/${i}_R2.fastq.gz | samtools view -@ 20 -bS - | samtools sort -@ 20 - -o ${folder}/hisat2/${i}_sorted.bam) 2>> ${folder}/hisat2/hisat2.log
+
+# Index sorted bam file for browser
+samtools index -@ 20 ${folder}/hisat2/${i}_sorted.bam
+
+# For count matrix generation
+echo "${folder}/hisat2/${i}_sorted.bam">>${folder}/nutshell/bam_files.txt
+
+done
